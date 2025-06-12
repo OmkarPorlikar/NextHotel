8e94b55b53df023e0421e55c752fa98aca45641e
OmkarPorlikar <128311037+OmkarPorlikar@users.noreply.github.com>
2025-06-12 17:57:38 +0530

    Update FloatingContactIcons.jsx

--- src/components/Home/FloatingContactIcons.jsx
+++ src/components/Home/FloatingContactIcons.jsx
@@ -20,7 +20,7 @@ const FloatingContactIcons = () => {
           alt="Phone Icon"
           width={40}
           height={40}
-          className="text-white w-[8.5] h-[8.5]"
+          className="text-white w-[9] h-[9]"
         />
       </a>
 
@@ -40,7 +40,7 @@ const FloatingContactIcons = () => {
           alt="WhatsApp Icon"
           width={40}
           height={40}
-          className="text-white w-[8.5] h-[8.5]"
+          className="text-white w-[9] h-[9]"
         />
       </a>
 
@@ -60,7 +60,7 @@ const FloatingContactIcons = () => {
           alt="Instagram Icon"
           width={40}
           height={40}
-          className="text-white w-[8.5] h-[8.5] rounded-[50%]"
+          className="text-white w-[9] h-[9] rounded-[50%]"
         />
       </a>
     </div>
