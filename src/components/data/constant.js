fc35363fc8670b3c91ec90c746121d8a7ecb5ced
OmkarPorlikar <128311037+OmkarPorlikar@users.noreply.github.com>
2025-06-12 17:47:24 +0530

    Update constant.js

--- src/components/data/constant.js
+++ src/components/data/constant.js
@@ -1,5 +1,5 @@
-    const phoneNumber = '+919876543210'; // Replace with real number
-    const whatsappNumber = '919876543210'; // No '+' in wa.me link
+    const phoneNumber = '+919021431127'; // Replace with real number
+    const whatsappNumber = '919021431127'; // No '+' in wa.me link
     const email = 'contact@gsrhotel.com';
 
     export {phoneNumber , whatsappNumber , email};
